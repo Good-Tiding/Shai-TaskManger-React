cd66e487416b6b145e61c78a4c672acbda7aab21
Good-Tiding <yorka22.business@gmail.com>
2024-11-02 17:43:35 +0300

    Updates

--- src/components/PrivateRoute.js
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-
-const PrivateRoute = ({ element, isAuthenticated }) => {
-  return isAuthenticated ? element : <Navigate to="/not-found" />;
-};
-
-export default PrivateRoute;
